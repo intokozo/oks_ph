d3b054d7f3480a6244e88a3118ea9a4f9cf15785
intokozo <morozbar@bk.ru>
2022-02-24 21:36:16 +0300

    Add database variables to dotenv

--- db/actions_with_db.rb
+++ db/actions_with_db.rb
@@ -1,6 +1,6 @@
 require 'sequel'
 
-DB = Sequel.connect("postgres://#{ENV['DB_LOGIN']}:#{ENV['DB_PASSWORD']}@localhost/photobase")
+DB = Sequel.connect("postgres://#{ENV['DB_LOGIN']}:#{ENV['DB_PASSWORD']}@#{ENV['DB_HOST']}/#{ENV['DB_NAME']}")
 
 unless DB.tables.include?(:categories)
   DB.create_table :categories do
